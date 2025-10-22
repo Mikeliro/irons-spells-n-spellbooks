6e6c947792bfeffe18aaf2e812e0adb4e4176a3b
iron431 <34083081+iron431@users.noreply.github.com>
2025-10-22 13:46:19 -0500

    unify name to undead rift

--- src/main/java/io/redspace/ironsspellbooks/setup/ClientSetup.java
+++ src/main/java/io/redspace/ironsspellbooks/setup/ClientSetup.java
@@ -18,7 +18,7 @@ import io.redspace.ironsspellbooks.block.scroll_forge.ScrollForgeRenderer;
 import io.redspace.ironsspellbooks.effect.PlanarSightEffect;
 import io.redspace.ironsspellbooks.entity.VisualFallingBlockRenderer;
 import io.redspace.ironsspellbooks.entity.mobs.dead_king_boss.DeadKingRenderer;
-import io.redspace.ironsspellbooks.entity.mobs.dead_king_boss.undead_spawner.UndeadSpawnPortalRenderer;
+import io.redspace.ironsspellbooks.entity.mobs.dead_king_boss.undead_spawner.UndeadRiftRenderer;
 import io.redspace.ironsspellbooks.entity.mobs.debug_wizard.DebugWizardRenderer;
 import io.redspace.ironsspellbooks.entity.mobs.frozen_humanoid.FrozenHumanoidRenderer;
 import io.redspace.ironsspellbooks.entity.mobs.horse.SpectralSteedRenderer;
@@ -345,7 +345,7 @@ public class ClientSetup {
         event.registerEntityRenderer(EntityRegistry.FROST_FIELD.get(), NoopRenderer::new);
         event.registerEntityRenderer(EntityRegistry.SNOWBALL.get(), SnowballRenderer::new);
         event.registerEntityRenderer(EntityRegistry.OMINOUS_FIRE_ORB.get(), FireOrbRenderer::new);
-        event.registerEntityRenderer(EntityRegistry.UNDEAD_PORTAL_WIP.get(), UndeadSpawnPortalRenderer::new);
+        event.registerEntityRenderer(EntityRegistry.UNDEAD_RIFT.get(), UndeadRiftRenderer::new);
 
         event.registerBlockEntityRenderer(BlockRegistry.SCROLL_FORGE_TILE.get(), ScrollForgeRenderer::new);
         event.registerBlockEntityRenderer(BlockRegistry.PEDESTAL_TILE.get(), PedestalRenderer::new);
