771f291f6fb9959aa0152d483e95e82fd9388589
iron431 <34083081+iron431@users.noreply.github.com>
2025-10-29 18:17:18 -0500

    particles and tweaks

--- src/main/java/io/redspace/ironsspellbooks/mixin/ParticleAccessor.java
+++ src/main/java/io/redspace/ironsspellbooks/mixin/ParticleAccessor.java
@@ -7,15 +7,21 @@ import org.spongepowered.asm.mixin.gen.Accessor;
 @Mixin(Particle.class)
 public interface ParticleAccessor {
     @Accessor("xo")
-    void irons_spellbooks$xo(double xo);
+    void irons_spellbooks$xo(double d);
     @Accessor("yo")
-    void irons_spellbooks$yo(double yo);
+    void irons_spellbooks$yo(double d);
     @Accessor("zo")
-    void irons_spellbooks$zo(double zo);
+    void irons_spellbooks$zo(double d);
     @Accessor("gravity")
-    void irons_spellbooks$gravity(float x);
+    void irons_spellbooks$gravity(float f);
     @Accessor("stoppedByCollision")
     void irons_spellbooks$stoppedByCollision(boolean b);
-    @Accessor("z")
-    void irons_spellbooks$z(double z);
+    @Accessor("stoppedByCollision")
+    boolean irons_spellbooks$isStoppedByCollision();
+    @Accessor("xd")
+    void irons_spellbooks$xd(double d);
+    @Accessor("yd")
+    void irons_spellbooks$yd(double d);
+    @Accessor("zd")
+    void irons_spellbooks$zd(double d);
 }
